5ac938e9d51a8c6cec2fd95e9ab75b66a8b02fe5
gokulnath panneerselvam <thunderbirdceg@gmail.com>
2024-06-12 12:01:13 +0530

    Expanding cards

---
+++ expanding-cards/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach(panel=>{
+    panel.addEventListener('click',()=>{
+        removeClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeClasses(){
+panels.forEach(panel=>{
+    panel.classList.remove('active')
+})
+}
